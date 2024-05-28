1e80ff83e9b12b6e629149743d17d6f99b110c04
Ray Kao <860691+raykao@users.noreply.github.com>
2024-05-28 00:40:11 +0000

    update: GH User name not needed as originally needed for public key

--- infra/terraform/01-main.tf
+++ infra/terraform/01-main.tf
@@ -9,23 +9,13 @@ terraform {
       version = "3.104.2"
     }
   }
-}
-
-provider "http" {
-  # Configuration options
-}
-
-data "http" "github_public_keys" {
-  url = "https://api.github.com/users/${var.GITHUB_USER}/keys"
-
-  # Optional request headers
-  request_headers = {
-    Accept = "application/json"
+  backend "azurerm" {
+    key                   = "terraform.tfstate"
   }
 }
 
+
 locals {
-  ssh_public_key = data.http.github_public_keys.body[0]
   tags = {
     env = "demo"
   }
