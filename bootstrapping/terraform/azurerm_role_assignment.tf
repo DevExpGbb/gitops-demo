a2dd092e04ea5dda11f6d6e437c6015f33965d8a
Ray Kao <860691+raykao@users.noreply.github.com>
2024-05-28 04:53:34 +0000

    fix: Owner or User Access Administrator required to assign MSIs their proper roles

--- bootstrapping/terraform/azurerm_role_assignment.tf
+++ bootstrapping/terraform/azurerm_role_assignment.tf
@@ -1,6 +1,6 @@
 resource "azurerm_role_assignment" "github-workload-identity" {
   scope                = azurerm_resource_group.workload-identity.id
-  role_definition_name = "Contributor"
+  role_definition_name = "Owner"
   principal_id         = azuread_service_principal.github-workload-identity.object_id
 }
 
