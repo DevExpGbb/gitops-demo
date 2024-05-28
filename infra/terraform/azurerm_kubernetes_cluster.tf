667627121cc3e5b8da31d7f3f8513108499fa546
Ray Kao <860691+raykao@users.noreply.github.com>
2024-05-28 04:46:16 +0000

    fix: subnet is not needed

--- infra/terraform/azurerm_kubernetes_cluster.tf
+++ infra/terraform/azurerm_kubernetes_cluster.tf
@@ -17,7 +17,7 @@ resource "azurerm_kubernetes_cluster" "cluster01" {
         object_id = azurerm_user_assigned_identity.kubelet.principal_id
         user_assigned_identity_id = azurerm_user_assigned_identity.kubelet.id
     }
-    
+
     default_node_pool {
         name = "systemnp01"
         node_count = var.default_node_pool.node_count
@@ -31,7 +31,6 @@ resource "azurerm_kubernetes_cluster_node_pool" "np01" {
     node_count = var.default_node_pool.node_count
     vm_size = var.default_node_pool.vm_size
     os_disk_size_gb = 30
-    vnet_subnet_id = azurerm_subnet.aks.id
     enable_auto_scaling = true
     min_count = 1
     max_count = 3
