19af436cb02f70131ea1db34064e6915d5f9cf05
Ray Kao <860691+raykao@users.noreply.github.com>
2024-05-28 04:26:45 +0000

    fix: Role required to read the container

--- bootstrapping/terraform/azurerm_role_assignment.tf
+++ bootstrapping/terraform/azurerm_role_assignment.tf
@@ -3,3 +3,9 @@ resource "azurerm_role_assignment" "github-workload-identity" {
   role_definition_name = "Contributor"
   principal_id         = azuread_service_principal.github-workload-identity.object_id
 }
+
+resource "azurerm_role_assignment" "github-workload-identity-storage-blob-data-contributor" {
+  scope                = azurerm_resource_group.workload-identity.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azuread_service_principal.github-workload-identity.object_id
+}
